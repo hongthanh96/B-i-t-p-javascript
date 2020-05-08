25a4ccde74fd729b73811f9487de04ced5f3da4b
hongthanh96 <hongthanh010896@gmail.com>
2020-05-08 14:23:29 +0700

    Bài tập sử dụng toán tử

---
+++ bt-khaibao-bien/bt-sudung-toantu/sudung-toantu.js
@@ -0,0 +1,4 @@
+let inputwidth = parseInt(prompt('Nhập chiều dài'));
+let inputheight = parseInt(prompt('Nhập chiều rộng'));
+let area = inputwidth * inputheight;
+document.write ('The area is: '+area);
