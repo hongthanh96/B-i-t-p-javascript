2127518937d27c0a25687f9a34169ea29eeeb37d
hongthanh96 <hongthanh010896@gmail.com>
2020-05-09 20:22:18 +0700

    Bài tập switch-case

---
+++ cau-truc-dieu-kien/cau-truc-switch-case/bai1.js
@@ -0,0 +1,14 @@
+let browser = prompt('Browser');
+switch (browser){
+    case 'Edge':
+        alert('You have got the Edge');
+        break;
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera' :
+        alert('Okay we support these browsers too');
+        break;
+    default:
+        alert('We hope that this page looks ok!');
+}
