5cfa9b205a24f779362266f4dd8e1e86cde70d0e
hongthanh96 <hongthanh010896@gmail.com>
2020-05-09 19:38:26 +0700

    Nhập user và pass

---
+++ cau-truc-dieu-kien/luyen-tap-if-else2/luyen-tap-if-else2.js
@@ -0,0 +1,20 @@
+let userName = prompt("Who's there? ");
+if (userName =='Admin'){
+    let password = prompt('Password');
+    if (password == 'TheMaster'){
+        alert('Welcome!');
+    }
+    else if (password == null){
+        alert('Canceled.');
+    }
+        else{
+        alert('Wrong Password');
+         }
+}
+else if (userName == null){
+    alert('Canceled.');
+}
+    else{
+        alert("I don't know you");
+    }
+
